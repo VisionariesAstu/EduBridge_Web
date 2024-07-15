65f867393bef16431ebd238a6000b0b66ef1286d
ephyg <ephywaanofii@gmail.com>
2024-07-15 17:37:50 +0300

    [feat]: Add Verify Code page

---
+++ src/app/auth/verify/page.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import RightImage from "/public/RoleImage.png";
+import Logo from "/public/Logo.png";
+import { Button } from "@/components/UI/button";
+import Link from "next/link";
+import { InputWithIcon } from "@/components/UI/input";
+import {
+  CodeIcon,
+  EnvelopeClosedIcon,
+  EnvelopeOpenIcon,
+} from "@radix-ui/react-icons";
+const Verify = () => {
+  return (
+    <div className="flex w-full min-h-screen bg-[url('/texture.png')] bg-white text-black">
+      <div className="flex justify-center  h-screen w-1/2 max-lg:hidden p-5">
+        <img
+          src={RightImage.src}
+          alt=""
+          className="w-full object-cover rounded-lg"
+        />
+      </div>
+      <div className="flex flex-col items-center justify-center w-1/2  p-32 max-lg:w-full max-md:p-10">
+        <div className="flex justify-between w-full items-center mb-10">
+          <div className="flex flex-col">
+            <span className="text-primary font-bold">Hello!</span>
+            <span className="text-gray-700 text-sm">Verify Your Code</span>
+          </div>
+          <div className="flex">
+            <img src={Logo.src} alt="" className="w-14" />
+          </div>
+        </div>
+        <div className="flex flex-col gap-3 w-full mb-5">
+          <InputWithIcon
+            icon={<EnvelopeOpenIcon color="gray" />}
+            type="text"
+            placeholder="OTP Code"
+            className=""
+          />
+        </div>
+        <Button className="w-full mb-3">Verify Code</Button>
+        <p className="text-sm">
+          Do you remember your password?
+          <span>
+            <Link href="/auth/login" className="text-[#44C079]">
+              {" "}
+              Login
+            </Link>
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Verify;
